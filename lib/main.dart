3e842a0e53ff5deb2a564a10a64c91f77752ae2f
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-04-22 00:46:42 +0200

    design

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Schyler'),
       home: HomePage(),
     ),
   );
@@ -31,97 +32,157 @@ class _HomePageState extends State<HomePage> {
           icon: const Icon(Icons.menu, color: Colors.black87),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                bottom: Radius.circular(30),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(30),
+                ),
               ),
-            ),
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Find your',
-                  style: const TextStyle(color: Colors.black87, fontSize: 25),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  'Inspiration',
-                  style: TextStyle(color: Colors.black87, fontSize: 40),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  padding: const EdgeInsets.only(
-                      left: 15, right: 15, bottom: 6, top: 6),
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(244, 243, 243, 1),
-                    borderRadius: BorderRadius.circular(25),
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Find your',
+                    style: const TextStyle(color: Colors.black87, fontSize: 25),
                   ),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      prefixIcon: Icon(
-                        Icons.search,
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    'Inspiration',
+                    style: TextStyle(
                         color: Colors.black87,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                        left: 15, right: 15, bottom: 6, top: 6),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(244, 243, 243, 1),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black87,
+                        ),
+                        hintText: "Search you're looking for",
+                        hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
                       ),
-                      hintText: "Search you're looking for",
-                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Promo Today',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 200,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      promoCard(''),
-                    ],
+                  const SizedBox(
+                    height: 10,
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Promo Today',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        promoCard('assets/images/one.jpg'),
+                        promoCard('assets/images/two.jpg'),
+                        promoCard('assets/images/three.jpg'),
+                        promoCard('assets/images/four.jpg'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 130,
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                          image: AssetImage('assets/images/three.jpg'),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          stops: const [0.3, 0.9],
+                          colors: [
+                            Colors.black.withOpacity(0.8),
+                            Colors.black.withOpacity(0.2)
+                          ],
+                        ),
+                      ),
+                      child: const Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Padding(
+                          padding: EdgeInsets.all(20),
+                          child: Text(
+                            'Best design',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget promoCard(image) {
     return AspectRatio(
-      aspectRatio: 2 / 3,
+      aspectRatio: 2.62 / 3,
       child: Container(
-        decoration: const BoxDecoration(color: Colors.orange),
+        margin: const EdgeInsets.only(right: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              stops: const [0.1, 0.9],
+              colors: [
+                Colors.black.withOpacity(0.8),
+                Colors.black.withOpacity(0.1)
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
