7f73089c7ea0923faa46d50746c0b3623308dcba
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-04-21 15:32:38 +0200

    design

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:HomePage(),
-    )
-  );
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -19,7 +19,106 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Color.fromRGBO(244, 243, 243, 1),
+      backgroundColor: const Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu, color: Colors.black87),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30))),
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Find your',
+                  style: const TextStyle(color: Colors.black87, fontSize: 25),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  'Inspiration',
+                  style: TextStyle(color: Colors.black87, fontSize: 40),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                      left: 15, right: 15, bottom: 6, top: 6),
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(244, 243, 243, 1),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.black87,
+                      ),
+                      hintText: "Search you're looking for",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Promo Today',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      promoCard(''),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget promoCard(image) {
+   return AspectRatio(
+      aspectRatio: 2 / 3,
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.orange),
+      ),
     );
   }
 }
